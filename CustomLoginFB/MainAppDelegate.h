a61ffcf4f505a0ecc8ad7cdd069fb523d6f44140
Rahul Morade <rahul@maxdigi.com>
2014-08-07 12:07:03 +0530

    Initial Commit

---
+++ CustomLoginFB/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  CustomLoginFB
+//
+//  Created by Rahul on 07/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
